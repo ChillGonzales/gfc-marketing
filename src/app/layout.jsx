d7df13b2af6094a6043234a361ecdcef293abd0e
Colin Monroe <colin@cryptocaddy.us>
2024-10-08 07:11:15 -0400

    add pixel

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,9 @@
 import { Merriweather, Open_Sans } from "next/font/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
-import Navbar from "@/app/components/traditional/navbar";
 import Footer from "@/app/components/traditional/footer";
 import { ThemeProvider } from "@/app/contexts/theme-provider";
+import Script from "next/script";
 
 const merriweather = Merriweather({
   style: ["normal", "italic"],
@@ -23,9 +23,35 @@ export const metadata = {
     "Find out about how SEO really works, and why you should stop paying for SEO work until you learn this.",
 };
 
+const PIXEL_ID = process.env.NEXT_PUBLIC_PIXEL_ID;
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script id={"facebook-pixel"}>
+          {`
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${PIXEL_ID}');
+                fbq('track', 'PageView');
+                `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1132885104625537&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body
         className={`${openSans.className} ${merriweather.variable} ${openSans.variable} bg-grey-800`}
       >
