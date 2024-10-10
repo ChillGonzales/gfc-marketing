d85380c2a2c0c34e29bcb43efea35682688e92c8
Colin Monroe <colin@cryptocaddy.us>
2024-10-10 12:06:10 -0400

    revertt

--- src/app/lp/ls/page.jsx
+++ src/app/lp/ls/page.jsx
@@ -3,11 +3,9 @@ import SeoMetadata from "@/app/components/seo-metadata";
 
 export default function LandscaperLandingPage() {
   return (
-    <LandingPage callout={"LANDSCAPERS"} niche={"landscaping business"}>
-      <SeoMetadata
-        title={"Get our FREE Course Now"}
-        description={"Register for our FREE Course and get instant access."}
-      />
-    </LandingPage>
+    <LandingPage
+      callout={"LANDSCAPERS"}
+      niche={"landscaping business"}
+    ></LandingPage>
   );
 }
